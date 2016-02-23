95753588b83353f9a3dab9fa8a90e3bc9cdc3210
shejunzhou <965678322@qq.com>
2016-02-23 17:11:30 +0800

    添加注释

--- SJWifiDemo/Model/SJWifiManager.h
+++ SJWifiDemo/Model/SJWifiManager.h
@@ -11,8 +11,26 @@
 @interface SJWifiManager : NSObject
 
 + (SJWifiManager *)sharedManager;
+
+/**
+ @method    isWiFiEnabled
+ @abstract  判断 wifi 是否开启
+ @return    YES/NO
+ */
 - (BOOL)isWiFiEnabled;
+
+/**
+ @method    wifiName
+ @abstract  获取连接的 wifi 名称
+ @return    连接的 wifi 名称
+ */
 - (NSString *)wifiName;
+
+/**
+ @method    localIPAddress
+ @abstract  获取连接上 wifi 的当前 ip地址
+ @return    本地 ip 地址
+ */
 - (NSString *)localIPAddress;
 
 @end
